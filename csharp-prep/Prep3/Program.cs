13d04cbcada52f62c90ff028a20a0f078acb26e2
yourusername <youremail@>
2024-09-21 15:22:18 -0600

    homework #3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+
+        int guess_number = -1;
+
+        while (guess_number != magicNumber)
+        {
+            Console.Write ("What is your guess? ");
+            guess_number = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess_number)
+            {
+                Console.WriteLine("Go higher");
+            }
+
+            else if  (magicNumber < guess_number)
+            {
+                Console.WriteLine("Go lower");
+            }
+
+            else
+            {
+                Console.WriteLine("Excellent you guessed the number!");
+            }
+
+        }
+
     }
 }
